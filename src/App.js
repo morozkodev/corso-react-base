bb65fc15b5800b22c71357fed74e9513cca34ce0
Morozko <fugerit.develop@gmail.com>
2020-12-05 15:09:28 +0100

    esercizio01 - MyComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      <MyComponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <h1>Sono MyComponent</h1>
+            <p>Sono un paragrafo</p>
+         </div>
+        )
+    }
+
+}
+
+export default MyComponent;
