47005492962427f58a95ad1ec6ece56948e38062
Morozko <fugerit.develop@gmail.com>
2020-12-06 11:42:00 +0100

    Test condizione
    Component : controllo-login/Bevenuto.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Benvenuto from './components/controllo-login/Benvenuto';
+
 function App() {
   return (
     <div className="App">
+	  <Benvenuto utenteAutenticato={false}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
