de68833100792a8c7b441472083541f12282b785
Morozko <fugerit.develop@gmail.com>
2020-12-06 12:21:15 +0100

    Esempio carrello bootstrap
    Component : carrello/Header, carrello/Menu, carrello/Prodotti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from './components/carrello/Header.js';
+import Menu  from './components/carrello/Menu.js';
+import Prodotti  from './components/carrello/Prodotti.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { carrello: []};
+  }
+
+  aggiornaCarrello( prodotto ) {
+  
+  }
+
+
+  render() {
+    const vocimenu = [
+      {id: 1, nome: 'Home', link: '/'},
+      {id: 2, nome: 'Prodotti', link: '/prodotti'},
+      {id: 3, nome: 'Contatti', link: '/contatti'},
+    ]
+    const prodotti = [
+      {id: 1, nome: 'Prodotto 1', prezzo: 11, img: { src: 'https://placeimg.com/320/240/tech', alt: 'prodotto1'}},
+      {id: 2, nome: 'Prodotto 2', prezzo: 12, img: { src: 'https://placeimg.com/320/240/tech', alt: 'prodotto2'}},
+      {id: 3, nome: 'Prodotto 3', prezzo: 13, img: { src: 'https://placeimg.com/320/240/tech', alt: 'prodotto3'}},
+      {id: 4, nome: 'Prodotto 4', prezzo: 14, img: { src: 'https://placeimg.com/320/240/tech', alt: 'prodotto4'}},
+    ]
+    
+    return (
+      <Container>
+          <Header titolo='Primo Sito React' />
+          <Menu voci={vocimenu} />
+          <Row>
+            <Col md={8}>
+              <Prodotti prodotti={prodotti} onChangeCarrello={this.aggiornaCarrello}/>
+            </Col>
+            <Col md={4}></Col>
+          </Row>
+      </Container>
+    )
+  }
+
 }
-
+ 
 export default App;
