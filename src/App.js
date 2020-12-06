4e0ade546b0f8ef1aae9f1f9b9dcdeafc0bd567e
Morozko <fugerit.develop@gmail.com>
2020-12-06 11:13:48 +0100

    Scheda prodotto base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import SchedaProdotto from './components/SchedaProdotto';
+
 function App() {
   return (
     <div className="App">
+	  <SchedaProdotto/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
