d1282238d66eff5d35beeb86c6ffc422ccbe65d4
Morozko <fugerit.develop@gmail.com>
2020-12-06 12:02:31 +0100

    Esempio lista
    Component : Liste/Lista.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Lista from './components/Liste/Lista';
+
 function App() {
   return (
     <div className="App">
+	  <Lista/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
