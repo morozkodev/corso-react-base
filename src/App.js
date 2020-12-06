09ae9b804713c5c2ed60ce9250e278869db9058c
Morozko <fugerit.develop@gmail.com>
2020-12-06 11:58:44 +0100

    Evento login e logout custom
    Componente : controllo-login/ControlloCustom.js
    
    Variazione con un unico bottone a cui viene personalizzata l'etichetta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ControlloLogin from './components/controllo-login/ControlloLogin';
+import ControlloCustom from './components/controllo-login/ControlloCustom';
 
 function App() {
   return (
     <div className="App">
-	  <ControlloLogin/>
+	  <ControlloCustom/>
       <br/><br/><br/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
