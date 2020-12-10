7eca9fa1adb35cb1105e117cbbba28ea6e927da0
Morozko <fugerit.develop@gmail.com>
2020-12-10 20:08:54 +0100

    Funzione Yup personalizzata

--- src/components/common/UserSchema.js
+++ src/components/common/UserSchema.js
@@ -3,8 +3,8 @@ import YupCommon from './YupCommon';
 
 export const schemaUser = Yup.object().shape(
 	{
-		name: Yup.string().min( 2 ).max( 10 ).required(),
-		username: Yup.string().min( 2 ).max( 10 ).required(),
+		name: Yup.string().verificaLunghezza( 2, 10, 'Nome' ).required(),
+		username: Yup.string().min(2).max(10).required(),
 		email: Yup.string().email().required()
 	}
 );
--- src/components/common/YupCommon.js
+++ src/components/common/YupCommon.js
@@ -1,4 +1,13 @@
 import { setLocale } from 'yup';
+import * as Yup from 'yup';
+
+Yup.addMethod(Yup.string, "verificaLunghezza", function ( min, max, label ) {
+  const msg = `La lunghezza ${label != undefined ? 'del campo '+label : ''} deve essere compresa tra ${min} e ${max} caratteri`;
+  return Yup.mixed().test(`verificaLunghezza`, msg, function( value ) {
+    const { path, createError } = this;
+    return value.length >= min  || createError({ path, msg });
+  })
+})
 
 setLocale({
 	mixed: {
@@ -10,3 +19,4 @@ setLocale({
 		max: 'Valore troppo lungo (Massimo ${max} caratteri)'
 	}
 });
+
