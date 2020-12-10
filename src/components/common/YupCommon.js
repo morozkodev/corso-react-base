4e08d6041b67fa2c0895b72f1f8e4b3cf1f1443f
Morozko <fugerit.develop@gmail.com>
2020-12-10 20:12:52 +0100

    Funzione Yup personalizzata

--- src/components/common/YupCommon.js
+++ src/components/common/YupCommon.js
@@ -2,10 +2,10 @@ import { setLocale } from 'yup';
 import * as Yup from 'yup';
 
 Yup.addMethod(Yup.string, "verificaLunghezza", function ( min, max, label ) {
-  const msg = `La lunghezza ${label != undefined ? 'del campo '+label : ''} deve essere compresa tra ${min} e ${max} caratteri`;
+  const msg = `La lunghezza ${label !== undefined ? 'del campo '+label : ''} deve essere compresa tra ${min} e ${max} caratteri`;
   return Yup.mixed().test(`verificaLunghezza`, msg, function( value ) {
     const { path, createError } = this;
-    return value.length >= min  || createError({ path, msg });
+    return ( value.length >= min && value.length <= max ) || createError({ path, msg });
   })
 })
 
