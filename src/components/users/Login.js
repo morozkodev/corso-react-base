5270df17f95b5d162a6e553bb30ad5ff2ede33bf
Morozko <fugerit.develop@gmail.com>
2020-12-11 16:08:38 +0100

    Componente esterno

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -5,17 +5,34 @@ import { ButtonBackToList } from './UserButton';
 class Login extends React.Component{
     constructor(props){
         super(props);
+		this.state = { codice: 'Codice da sostituire' }; 
 		this.auth = this.auth.bind( this );
+		this.test = this.test.bind( this );
     }
     auth(){
         this.props.onAuth();
     }
+	
+    test(){
+        import('./Test')
+            .then((Test)=>{
+                console.log('ok');
+				this.setState( { codice: Test.default.render() } );
+            })
+            .catch(err=>{
+                console.log(err);
+            });
+    }
+
     render(){
         return(
             <section>
                 <h1>Login</h1>
                 <button onClick={this.auth}>Login</button>
-				<br/><br/>
+				<button onClick={this.test}>Test</button>
+				<br/>
+				{this.state.codice}
+				<br/>
 				<ButtonBackToList />
             </section>
         )
---
+++ src/components/users/Test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { ButtonBackToList } from './UserButton';
+
+class Test extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    static render(){
+        return(
+            <section>
+                <h1>Test</h1>
+            </section>
+        )
+    }
+}
+
+export default Test;
